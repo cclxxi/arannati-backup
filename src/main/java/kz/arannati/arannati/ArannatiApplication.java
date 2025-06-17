9ac51921e0e7d980be4c967a320f028f243721da
Ilia_P <ilia.proshin82@gmail.com>
2025-06-17 14:15:27 +0600

    initial commit

---
+++ src/main/java/kz/arannati/arannati/ArannatiApplication.java
@@ -0,0 +1,13 @@
+package kz.arannati.arannati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArannatiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArannatiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/arannati/arannati/ArannatiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.arannati.arannati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArannatiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
