72196fa7520b10214d3c9d5141aff429e9c00e51
Ilia_P <ilia.proshin82@gmail.com>
2025-06-20 14:42:43 +0600

    feat(config): add WebConfig for content negotiation and resource handling, update SecurityConfig
    - Introduce `WebConfig` to configure content negotiation for SVG media types and static resource handling with caching.
    - Remove redundant CSRF disabling in `SecurityConfig`.

--- src/main/java/kz/arannati/arannati/config/SecurityConfig.java
+++ src/main/java/kz/arannati/arannati/config/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/auth/**").permitAll()
